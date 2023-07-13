871447996198764fe95ddc241e6a80f61649a8a3
giuliabruno05 <giuliabruno48@gmail.com>
2023-07-14 00:30:43 +0200

    first commit

---
+++ database/migrations/2023_07_13_215706_create_railways_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('railways', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',60);
+            $table->string('stazione_di_partenza',60);
+            $table->string('stazione_di_arrivo',60);
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->integer('codice_treno');
+            $table->tinyInteger('numero_carozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('railways');
+    }
+};

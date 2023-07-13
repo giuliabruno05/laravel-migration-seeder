64d16f8404caf62ca68395acb852debcdd4811f0
giuliabruno05 <giuliabruno48@gmail.com>
2023-07-14 01:26:22 +0200

    creazione seeder e richiamato nel file db seeder

--- database/seeders/RailwayTableSeeder.php
+++ database/seeders/RailwayTableSeeder.php
@@ -5,15 +5,17 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+use App\Models\Railway;
+
+class RailwayTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([ RailwayTableSeeder :: class]);
+       Railway :: factory() -> count(15) -> create();
     }
 }
